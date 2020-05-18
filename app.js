ba3d73c7a094d1d7239c7ca2a6f2b70b8e4f6035
Yauheni Sheuchuk <shevchuked@tut.by>
2020-05-18 17:10:00 +0300

    add section with data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 window.addEventListener('popstate', (e) => {
-	console.log('popstate event ', e);
+	console.log('popstate event ', e.detail.id);
+	bookPreview(e.detail.id);
 });
 
 window.onload = () => {
@@ -22,6 +23,7 @@ window.onload = () => {
 	})
 
 	root.appendChild(nav);
+	root.appendChild(section);
 }
 
 function createItemBookList (name, id) {
@@ -52,4 +54,37 @@ function clickLink(e) {
 	const newHref = baseUrl	 + `?id=${this.id}#preview`;
 	window.history.pushState({}, '', newHref);
 	window.dispatchEvent(new CustomEvent('popstate', {'detail': {'id': this.id}}));
+}
+
+function bookPreview(id) {
+	const idToNumber = Number(id);
+	const section = document.querySelectorAll('.section')[0];
+	section.innerHTML = '';
+	const header = document.createElement('h3');
+	header.innerText = 'Book preview!';
+	const book = books.find((elem) => elem.id === idToNumber);
+	section.appendChild(header);
+	section.appendChild(createBookBlock(book));
+
+}
+
+function createBookBlock (book) {
+	const container = document.createElement('div');
+	container.setAttribute('class', 'bookContainer');
+	const bookName = document.createElement('p');
+	bookName.setAttribute('class', 'bookName');
+	bookName.innerHTML = `<b>Book name:<b> ` + book.name;
+	const author = document.createElement('p');
+	author.setAttribute('class', 'bookAuthor');
+	author.innerHTML = `<b>Book author:<b> ` + book.author;
+	const plot = document.createElement('p');
+	plot.setAttribute('class', 'bookPlot');
+	plot.innerHTML = `<b>Book plot:<b> ` + book.plot;
+	const img = document.createElement('img');
+	img.setAttribute('src', `${book.image}`);
+	container.appendChild(bookName);
+	container.appendChild(author);
+	container.appendChild(plot);
+	container.appendChild(img);
+	return container;
 }
